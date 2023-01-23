eee242c97c18b111f18915c41ebbe1f70a69d549
pauwma <paumual2003@elpuig.xeill.net>
2023-01-23 17:55:40 +0100

    Clase 23/01 17:55

--- src/schema.sql
+++ src/schema.sql
@@ -1,70 +1,69 @@
-CREATE TABLE Jugador (
-idJugador INT PRIMARY KEY CONSTRAINT idJugador NOT NULL,
-rank TEXT,
-wins TEXT,
-kills TEXT,
-deaths TEXT,
-assists TEXT,
-scoreround TEXT,
-kad TEXT,
-killsround TEXT,
-plants TEXT,
-firstbloods TEXT,
-clutches TEXT,
-flawless TEXT,
-aces TEXT
+CREATE TABLE location (
+	location_name TEXT NOT NULL,
+	launch_location TEXT NOT NULL,
+	rockets_launched INTEGER NOT NULL,
+	PRIMARY KEY (location_name)
 );
 
-CREATE TABLE Mapa (
-idMapa INT PRIMARY KEY,
-idJugador INT,
-CONSTRAINT idjugadormapa FOREIGN KEY (idJugador) REFERENCES Jugador(idJugador)
-name TEXT,
-porcentaje_win TEXT,
-wins TEXT,
-losses TEXT,
-kd TEXT,
-adr TEXT,
-acs TEXT,
+CREATE TABLE mission (
+    mission_name TEXT NOT NULL,
+    mission_description TEXT NOT NULL,
+    mission_launch_cost TEXT NOT NULL,
+    mission_type TEXT NOT NULL,
+    PRIMARY KEY (mission_name)
 );
 
-CREATE TABLE Partida (
-idPartida INT PRIMARY KEY,
-idJugador INT,
-idMapa INT,
-CONSTRAINT idjugadorpartida FOREIGN KEY (idJugador) REFERENCES Jugador(idJugador),
-CONSTRAINT idmapapartida FOREIGN KEY (idMapa) REFERENCES Mapa(idMapa),
-name_map TEXT,
-type TEXT,
-result TEXT
+CREATE TABLE rocket (
+    rocket_name TEXT NOT NULL,
+    rocket_family TEXT NOT NULL,
+    rocket_length TEXT NOT NULL,
+    rocket_diameter TEXT NOT NULL,
+    rocket_low_earth_orbit_capacity TEXT NOT NULL,
+    rocket_launch_mass TEXT NOT NULL,
+    rocket_description TEXT NOT NULL,
+    PRIMARY KEY (rocket_name)
 );
 
-CREATE TABLE Agentes (
-idAgente INT PRIMARY KEY,
-idJugador INT,
-CONSTRAINT idjugadoragentes FOREIGN KEY (idJugador) REFERENCES Jugador(idJugador)
-name TEXT,
-type TEXT,
-timePlayed TEXT,
-matches TEXT,
-win TEXT,
-kd TEXT,
-adr TEXT,
-acs TEXT,
-hs TEXT,
-kast TEXT,
+ALTER TABLE mission ADD COLUMN rocket_name TEXT NOT NULL
+CONSTRAINT rocket_name REFERENCES rocket (rocket_name)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+CREATE TABLE agency (
+    agency_name TEXT NOT NULL,
+    agency_type TEXT NOT NULL,
+    agency_abbreviation TEXT NOT NULL,
+    agency_administration TEXT NOT NULL,
+    agency_founded TEXT NOT NULL,
+    agency_country TEXT NOT NULL,
+    agency_spacecraft TEXT NOT NULL,
+    agency_launchers TEXT NOT NULL,
+    agency_description TEXT NOT NULL,
+    PRIMARY KEY (agency_name)
 );
 
-CREATE TABLE Armas (
-idArma INT PRIMARY KEY,
-idJugador INT,
-CONSTRAINT idjugadorarmas FOREIGN KEY (idJugador) REFERENCES Jugador(idJugador)
-name TEXT,
-type TEXT,
-kills TEXT,
-deaths TEXT,
-headshots TEXT,
-damageRound TEXT,
-killsRound TEXT,
-longestKill TEXT
+ALTER TABLE rocket ADD COLUMN agency_name TEXT NOT NULL
+CONSTRAINT agency_name REFERENCES agency (agency_name)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+CREATE TABLE launch (
+	launch_title TEXT NOT NULL,
+	launch_status TEXT NOT NULL,
+	launch_date TEXT NOT NULL,
+	PRIMARY KEY (launch_title)
 );
+
+ALTER TABLE launch ADD COLUMN rocket_name TEXT NOT NULL
+CONSTRAINT rocket_name REFERENCES rocket (rocket_name)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE launch ADD COLUMN agency_name TEXT NOT NULL
+CONSTRAINT agency_name REFERENCES agency (agency_name)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE launch ADD COLUMN location_name TEXT NOT NULL
+CONSTRAINT location_name REFERENCES location (location_name)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE launch ADD COLUMN mission_name TEXT NOT NULL
+CONSTRAINT mission_name REFERENCES mission (mission_name)
+ON UPDATE CASCADE ON DELETE CASCADE;
